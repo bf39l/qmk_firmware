9caf5ebdefb844e70f70690fccacf31515f76ce5
Bo Liu <boliu1234@gmail.com>
2024-05-24 22:09:15 +0800

    tmp plank_full

--- keyboards/plank_full/config.h
+++ keyboards/plank_full/config.h
@@ -18,3 +18,163 @@
 //#define NO_ACTION_LAYER
 //#define NO_ACTION_TAPPING
 //#define NO_ACTION_ONESHOT
+
+
+/*
+ * Feature disable options
+ *  These options are also useful to firmware size reduction.
+ */
+
+/* disable debug print */
+//#define NO_DEBUG
+
+/* disable print */
+//#define NO_PRINT
+
+/* disable action features */
+//#define NO_ACTION_LAYER
+//#define NO_ACTION_TAPPING
+//#define NO_ACTION_ONESHOT
+
+// ------------------
+#define QUICK_TAP_TERM 0
+
+// #define SERIAL_SLAVE_BUFFER_LENGTH  ((MATRIX_ROWS)/2)
+// #define SERIAL_MASTER_BUFFER_LENGTH ((MATRIX_ROWS)/2)
+
+/* Select hand configuration */
+// #define MASTER_LEFT
+// #define MASTER_RIGHT
+// #define EE_HANDS
+
+
+/* key matrix size */
+// Rows are doubled-up
+// #define MATRIX_ROWS 10 // 5 row original
+// #define MATRIX_ROW_PINS {GP10, GP11, GP12, GP13, GP14}
+
+// wiring of each half
+// #define MATRIX_COLS 7
+// #define MATRIX_COL_PINS {GP2, GP3, GP4, GP5, GP6, GP8, GP9}
+
+/* COL2ROW, ROW2COL*/
+// #define DIODE_DIRECTION COL2ROW
+#define DEBOUNCE 3
+
+// #ifdef OLED_ENABLE
+// https://github.com/qmk/qmk_firmware/blob/master/docs/platformdev_rp2040.md
+#define I2C1_SDA_PIN  GP20
+#define I2C1_SCL_PIN  GP21
+#define I2C_DRIVER I2CD0
+#define OLED_DISPLAY_128X64
+
+#define OLED_BRIGHTNESS 128
+// #define OLED_FONT_H "keyboards/bf39l_xbows/lib/glcdfont.c"
+// #endif
+
+// #define SERIAL_USART_FULL_DUPLEX
+// #define SERIAL_USART_TX_PIN GP16
+// #define SERIAL_USART_RX_PIN GP17
+
+// #define HAL_USE_I2C TRUE
+// #define HAL_USE_SIO TRUE
+// #include_next <halconf.h>
+
+#include_next <mcuconf.h> // for i2c
+#undef RP_I2C_USE_I2C0
+#define RP_I2C_USE_I2C0 TRUE
+#undef RP_I2C_USE_I2C1
+#define RP_I2C_USE_I2C1 TRUE
+
+#define POINTING_DEVICE_AUTO_MOUSE_ENABLE
+// This enables transmitting the pointing device status
+// to the master side of the split keyboard.
+// The purpose of this feature is to enable use pointing
+// devices on the slave side.
+
+// #undef FORCE_NKRO
+// #define FORCE_NKRO
+
+// #define SPLIT_POINTING_ENABLE
+// #define POINTING_DEVICE_COMBINED
+#define AZOTEQ_IQS5XX_TPS65
+#define AZOTEQ_IQS5XX_ADDRESS 0xE8
+#define AZOTEQ_IQS5XX_TIMEOUT_MS 10
+// #define AZOTEQ_IQS5XX_WIDTH_MM 77
+// #define AZOTEQ_IQS5XX_HEIGHT_MM 48
+// #define POINTING_DEVICE_LEFT
+// #define AZOTEQ_IQS5XX_ACTIVE
+
+// #define AZOTEQ_IQS5XX_TPS65
+// #undef AZOTEQ_IQS5XX_WIDTH_MM
+// #define AZOTEQ_IQS5XX_WIDTH_MM 77
+// #undef AZOTEQ_IQS5XX_HEIGHT_MM
+// #define AZOTEQ_IQS5XX_HEIGHT_MM 48
+#define AZOTEQ_IQS5XX_TAP_ENABLE true
+#define AZOTEQ_IQS5XX_TWO_FINGER_TAP_ENABLE true
+#define AZOTEQ_IQS5XX_ZOOM_ENABLE true
+#define AZOTEQ_IQS5XX_PRESS_AND_HOLD_ENABLE true
+#define AZOTEQ_IQS5XX_SCROLL_ENABLE true
+
+
+
+
+
+
+
+/*
+
+// ------------------
+#define QUICK_TAP_TERM 0
+
+// #define MASTER_LEFT
+
+// COL2ROW, ROW2COL
+// #define DIODE_DIRECTION COL2ROW
+#define DEBOUNCE 3
+
+#define I2C0_SDA_PIN  GP20
+#define I2C0_SCL_PIN  GP21
+#define I2C1_SDA_PIN  GP20
+#define I2C1_SCL_PIN  GP21
+#define I2C_DRIVER I2CD0
+
+// #define HAL_USE_I2C TRUE
+#include_next <halconf.h>
+
+#include_next <mcuconf.h> // for i2c
+#undef RP_I2C_USE_I2C0
+#define RP_I2C_USE_I2C0 TRUE
+// #undef RP_I2C_USE_I2C1
+// #define RP_I2C_USE_I2C1 TRUE
+
+// #define SPLIT_POINTING_ENABLE
+// This enables transmitting the pointing device status
+// to the master side of the split keyboard.
+// The purpose of this feature is to enable use pointing
+// devices on the slave side.
+
+// pointing device
+#define CIRQUE_PINNACLE_ADDR 0x0A
+// #define POINTING_DEVICE_LEFT
+#define CIRQUE_PINNACLE_DIAMETER_MM 40
+#define CIRQUE_PINNACLE_TAP_ENABLE
+#define POINTING_DEVICE_GESTURES_SCROLL_ENABLE
+#define POINTING_DEVICE_DEBUG
+
+
+ // Feature disable options
+ // These options are also useful to firmware size reduction.
+
+// disable debug print 
+//#define NO_DEBUG
+
+// disable print
+//#define NO_PRINT
+
+// disable action features
+//#define NO_ACTION_LAYER
+//#define NO_ACTION_TAPPING
+//#define NO_ACTION_ONESHOT
+
+*/
--- keyboards/plank_full/keymaps/default/keymap.c
+++ keyboards/plank_full/keymaps/default/keymap.c
@@ -1,24 +1,133 @@
 // Copyright 2023 QMK
 // SPDX-License-Identifier: GPL-2.0-or-later
-
 #include QMK_KEYBOARD_H
+
+#include "i2c_master.h"
+#include "debug.h"
+
+#define TIMEOUT 50
+
+// TODO: remove patch
+#ifdef PROTOCOL_CHIBIOS
+#    pragma message("ChibiOS is currently 'best effort' and might not report accurate results")
+#endif
+
+#ifdef PROTOCOL_CHIBIOS
+#endif
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     /*
-     * ┌───┬───┬───┬───┐
-     * │ 7 │ 8 │ 9 │ / │
-     * ├───┼───┼───┼───┤
-     * │ 4 │ 5 │ 6 │ * │
-     * ├───┼───┼───┼───┤
-     * │ 1 │ 2 │ 3 │ - │
-     * ├───┼───┼───┼───┤
-     * │ 0 │ . │Ent│ + │
-     * └───┴───┴───┴───┘
-     */
-    [0] = LAYOUT_ortho_4x4(
-        KC_P7,   KC_P8,   KC_P9,   KC_PSLS,
-        KC_P4,   KC_P5,   KC_P6,   KC_PAST,
-        KC_P1,   KC_P2,   KC_P3,   KC_PMNS,
-        KC_P0,   KC_PDOT, KC_PENT, KC_PPLS
+    L: (r0,c0) DRAG_SCROLL (r0,c1) KC_P6
+       (r1,c0) KC_P1       (r1,c1) KC_P2
+    R: (r0,c0) KC_MS_BTN1  (r0,c1) KC_MS_BTN2
+       (r1,c0) KC_P3       (r1,c1) KC_P4
+
+       LALT(KC_RIGHT)
+       LT(2, KC_SPC)
+    */
+
+    [0] = LAYOUT_def(
+
+        KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,  KC_RBRC,
+        KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,  KC_LBRC,
+        KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,  KC_BSPC,
+        KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                 KC_ESC,
+        KC_RSFT, KC_LCTL, KC_LCTL, KC_LALT, KC_LGUI,        KC_SPC,        MO(2)
+
+    ),
+    [1] = LAYOUT_fn(
+        KC_TRNS, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F11,
+        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+        KC_CAPS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_DEL,
+        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,          KC_TRNS,
+        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,          KC_TRNS, KC_TRNS
+    ),
+    [2] = LAYOUT_mos(
+        KC_TRNS,    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, TO(3),
+        KC_TRNS,    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+        KC_TRNS,    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+        KC_TRNS,    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,          KC_TRNS,
+        KC_TRNS,    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,          KC_TRNS, KC_TRNS
+    ),
+    [3] = LAYOUT_conf(
+        QK_BOOT, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, TO(0),
+        NK_ON,   NK_OFF,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,          KC_TRNS,
+        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,          KC_TRNS, KC_TRNS
     )
 };
+
+#include "print.h"
+
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+  // If console is enabled, it will print the matrix position and status of each key pressed
+#ifdef CONSOLE_ENABLE
+    uprintf("KL: kc: 0x%04X, col: %2u, row: %2u, pressed: %u, time: %5u, int: %u, count: %u\n", keycode, record->event.key.col, record->event.key.row, record->event.pressed, record->event.time, record->tap.interrupted, record->tap.count);
+#endif 
+    return true;
+}
+
+void do_scan(void) {
+    uint8_t nDevices = 0;
+
+    dprintf("Scanning...\n");
+
+    for (uint8_t address = 1; address < 127; address++) {
+        // The i2c_scanner uses the return value of
+        // i2c_ping_address to see if a device did acknowledge to the address.
+        i2c_status_t error = i2c_ping_address(address << 1, TIMEOUT);
+        if (error == I2C_STATUS_SUCCESS) {
+            dprintf("  I2C device found at address 0x%02X\n", address);
+            nDevices++;
+        } else {
+            // dprintf("  Unknown error (%u) at address 0x%02X\n", error, address);
+        }
+    }
+
+    if (nDevices == 0)
+        dprintf("No I2C devices found\n");
+    else
+        dprintf("done\n");
+}
+
+uint16_t scan_timer = 0;
+
+void matrix_scan_user(void) {
+    if (timer_elapsed(scan_timer) > 5000) {
+        do_scan();
+        scan_timer = timer_read();
+    }
+}
+
+void keyboard_post_init_user(void) {
+    debug_enable = true;
+    debug_matrix = true;
+
+    i2c_init();
+    scan_timer = timer_read();
+}
+
+// report_mouse_t pointing_device_task_user(report_mouse_t mouse_report) {
+//     dprintf("pointing_device_task_user\n");
+
+//     return mouse_report;
+// }
+
+#ifdef OLED_ENABLE
+
+static void render_logo(void) {
+    static const char PROGMEM qmk_logo[] = {
+        0x80, 0x81, 0x82, 0x83, 0x84, 0x85, 0x86, 0x87, 0x88, 0x89, 0x8A, 0x8B, 0x8C, 0x8D, 0x8E, 0x8F, 0x90, 0x91, 0x92, 0x93, 0x94,
+        0xA0, 0xA1, 0xA2, 0xA3, 0xA4, 0xA5, 0xA6, 0xA7, 0xA8, 0xA9, 0xAA, 0xAB, 0xAC, 0xAD, 0xAE, 0xAF, 0xB0, 0xB1, 0xB2, 0xB3, 0xB4,
+        0xC0, 0xC1, 0xC2, 0xC3, 0xC4, 0xC5, 0xC6, 0xC7, 0xC8, 0xC9, 0xCA, 0xCB, 0xCC, 0xCD, 0xCE, 0xCF, 0xD0, 0xD1, 0xD2, 0xD3, 0xD4, 0x00
+    };
+
+    oled_write_P(qmk_logo, false);
+}
+
+bool oled_task_user(void) {
+    render_logo();
+    return false;
+}
+#endif
