aab7b9e7dc308e66de4a798e4bb0c3c56af370ca
Bo Liu <boliu1234@gmail.com>
2024-05-24 22:09:15 +0800

    tmp plank_full

---
+++ keyboards/plank_full/keymaps/default/rules.mk
@@ -0,0 +1,16 @@
+# Bootloader selection
+BOOTLOADER = rp2040
+
+# WPM_ENABLE = yes          # Enable WPM Counter
+OLED_ENABLE = yes          # Enable OLED
+OLED_DRIVER = ssd1306      # Enable Support for SSD1306 or SH1106 OLED Displays; Communicating over I2C
+OLED_TRANSPORT = i2c
+
+# SPLIT_KEYBOARD = yes
+
+# SERIAL_DRIVER = vendor
+
+POINTING_DEVICE_ENABLE = yes
+POINTING_DEVICE_DRIVER = azoteq_iqs5xx
+
+MOUSEKEY_ENABLE = yes
