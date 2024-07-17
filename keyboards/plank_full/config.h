7cb29927725a670b4072462b33b58d18ccf895f6
Bo Liu <boliu1234@gmail.com>
2024-07-17 22:45:52 +1200

    Finalise plank full keyboard
    - update azoteq_iqs5xx.c: allow user define AZOTEQ_IQS5XX_WIDTH_MM and AZOTEQ_IQS5XX_HEIGHT_MM
    - clean up plank full keyboard debug codes
    - small update for alix_split which allows the keyboard has hid

--- keyboards/plank_full/config.h
+++ keyboards/plank_full/config.h
@@ -61,6 +61,10 @@
 // #define DIODE_DIRECTION COL2ROW
 #define DEBOUNCE 3
 
+#define SERIAL_USART_FULL_DUPLEX
+#define SERIAL_USART_TX_PIN GP16
+#define SERIAL_USART_RX_PIN GP17
+
 // #ifdef OLED_ENABLE
 // https://github.com/qmk/qmk_firmware/blob/master/docs/platformdev_rp2040.md
 #define I2C1_SDA_PIN  GP20
@@ -69,21 +73,10 @@
 #define OLED_DISPLAY_128X64
 
 #define OLED_BRIGHTNESS 128
-// #define OLED_FONT_H "keyboards/bf39l_xbows/lib/glcdfont.c"
-// #endif
 
-// #define SERIAL_USART_FULL_DUPLEX
-// #define SERIAL_USART_TX_PIN GP16
-// #define SERIAL_USART_RX_PIN GP17
 
-// #define HAL_USE_I2C TRUE
-// #define HAL_USE_SIO TRUE
-// #include_next <halconf.h>
-
-// #include_next <mcuconf.h> // for i2c
-
-#define POINTING_DEVICE_DEBUG
-#define POINTING_DEVICE_AUTO_MOUSE_ENABLE
+// #define POINTING_DEVICE_DEBUG
+// #define POINTING_DEVICE_AUTO_MOUSE_ENABLE
 // This enables transmitting the pointing device status
 // to the master side of the split keyboard.
 // The purpose of this feature is to enable use pointing
@@ -92,88 +85,16 @@
 // #undef FORCE_NKRO
 // #define FORCE_NKRO
 
-// #define SPLIT_POINTING_ENABLE
-// #define POINTING_DEVICE_COMBINED
 #define AZOTEQ_IQS5XX_TPS65
 // #define AZOTEQ_IQS5XX_ADDRESS 0xE9
 #define AZOTEQ_IQS5XX_TIMEOUT_MS 10
 // #undef AZOTEQ_IQS5XX_WIDTH_MM
-// #define AZOTEQ_IQS5XX_WIDTH_MM 63
+#define AZOTEQ_IQS5XX_WIDTH_MM 63
 // #undef AZOTEQ_IQS5XX_HEIGHT_MM
-// #define AZOTEQ_IQS5XX_HEIGHT_MM 47
-// #define POINTING_DEVICE_LEFT
-// #define AZOTEQ_IQS5XX_ACTIVE
+#define AZOTEQ_IQS5XX_HEIGHT_MM 47
 
-// #define AZOTEQ_IQS5XX_TPS65
-// #undef AZOTEQ_IQS5XX_WIDTH_MM
-// #define AZOTEQ_IQS5XX_WIDTH_MM 77
-// #undef AZOTEQ_IQS5XX_HEIGHT_MM
-// #define AZOTEQ_IQS5XX_HEIGHT_MM 48
-#define AZOTEQ_IQS5XX_TAP_ENABLE true
-#define AZOTEQ_IQS5XX_TWO_FINGER_TAP_ENABLE true
-#define AZOTEQ_IQS5XX_ZOOM_ENABLE true
-#define AZOTEQ_IQS5XX_PRESS_AND_HOLD_ENABLE true
-#define AZOTEQ_IQS5XX_SCROLL_ENABLE true
-
-
-
-
-
-
-
-/*
-
-// ------------------
-#define QUICK_TAP_TERM 0
-
-// #define MASTER_LEFT
-
-// COL2ROW, ROW2COL
-// #define DIODE_DIRECTION COL2ROW
-#define DEBOUNCE 3
-
-#define I2C0_SDA_PIN  GP20
-#define I2C0_SCL_PIN  GP21
-#define I2C1_SDA_PIN  GP20
-#define I2C1_SCL_PIN  GP21
-#define I2C_DRIVER I2CD0
-
-// #define HAL_USE_I2C TRUE
-#include_next <halconf.h>
-
-#include_next <mcuconf.h> // for i2c
-#undef RP_I2C_USE_I2C0
-#define RP_I2C_USE_I2C0 TRUE
-// #undef RP_I2C_USE_I2C1
-// #define RP_I2C_USE_I2C1 TRUE
-
-// #define SPLIT_POINTING_ENABLE
-// This enables transmitting the pointing device status
-// to the master side of the split keyboard.
-// The purpose of this feature is to enable use pointing
-// devices on the slave side.
-
-// pointing device
-#define CIRQUE_PINNACLE_ADDR 0x0A
-// #define POINTING_DEVICE_LEFT
-#define CIRQUE_PINNACLE_DIAMETER_MM 40
-#define CIRQUE_PINNACLE_TAP_ENABLE
-#define POINTING_DEVICE_GESTURES_SCROLL_ENABLE
-#define POINTING_DEVICE_DEBUG
-
-
- // Feature disable options
- // These options are also useful to firmware size reduction.
-
-// disable debug print 
-//#define NO_DEBUG
-
-// disable print
-//#define NO_PRINT
-
-// disable action features
-//#define NO_ACTION_LAYER
-//#define NO_ACTION_TAPPING
-//#define NO_ACTION_ONESHOT
-
-*/
+#define AZOTEQ_IQS5XX_TAP_ENABLE false
+#define AZOTEQ_IQS5XX_TWO_FINGER_TAP_ENABLE false
+#define AZOTEQ_IQS5XX_ZOOM_ENABLE false
+#define AZOTEQ_IQS5XX_PRESS_AND_HOLD_ENABLE false
+#define AZOTEQ_IQS5XX_SCROLL_ENABLE false
--- keyboards/plank_full/keymaps/default/rules.mk
+++ keyboards/plank_full/keymaps/default/rules.mk
@@ -6,7 +6,7 @@ OLED_ENABLE = yes          # Enable OLED
 OLED_DRIVER = ssd1306      # Enable Support for SSD1306 or SH1106 OLED Displays; Communicating over I2C
 OLED_TRANSPORT = i2c
 
-CONSOLE_ENABLE = yes
+# CONSOLE_ENABLE = yes
 I2C_DRIVER_REQUIRED = yes
 
 # SPLIT_KEYBOARD = yes
@@ -17,3 +17,4 @@ POINTING_DEVICE_ENABLE = yes
 POINTING_DEVICE_DRIVER = azoteq_iqs5xx
 
 MOUSEKEY_ENABLE = yes
+RAW_ENABLE = yes # 
