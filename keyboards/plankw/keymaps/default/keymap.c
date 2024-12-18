fa61117db41454780c7b2cd8995164c5cb169a40
Bo Liu <boliu1234@gmail.com>
2024-12-19 10:40:53 +1300

    change alix-split keymap

--- keyboards/plankw/keymaps/default/keymap.c
+++ keyboards/plankw/keymaps/default/keymap.c
@@ -21,37 +21,37 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
     [0] = LAYOUT_def(
 
-        KC_GRV,         KC_1,    KC_2,    KC_3,    KC_4,    KC_5,  MEH_T(KC_ESC),
-        KC_TAB,         KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,  KC_LBRC,
-        HYPR_T(KC_ESC), KC_A,    KC_S,    KC_D,    KC_F,    KC_G,  KC_RBRC,
-        KC_LSFT,        KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                 MO(2),
-        KC_LCTL,        KC_LCTL, KC_LALT, KC_LALT, KC_LGUI,        KC_SPC,        KC_BSPC,
+        KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,  KC_RBRC,
+        KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,  KC_LBRC,
+        KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,  KC_BSPC,
+        KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                 KC_ESC,
+        KC_RSFT, KC_LCTL, KC_LCTL, KC_LALT, KC_LGUI,        KC_SPC,        MO(2),
 
 
-                     KC_DEL,         KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS,
-                     KC_BSLS,        KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_EQL,
-                     KC_QUOT,        KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_ENT,
-        KC_NO,                       KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
-        DRAG_SCROLL, MT(1, KC_SPC),        KC_RGUI, KC_RALT, KC_RCTL, TO(1),   TO(2)
+                     KC_NO,      KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS,
+                     KC_BSLS,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_EQL,
+                     KC_QUOT,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_ENT,
+        KC_NO,                   KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
+        DRAG_SCROLL, MO(1),               KC_RGUI, KC_RALT, KC_RCTL, TO(1),   TO(2)
 
     ),
     [1] = LAYOUT_fn(
         KC_TRNS, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F11,
         KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+        KC_CAPS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_DEL,
         KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,          KC_TRNS,
-        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,          KC_TRNS, KC_DEL,
+        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,          KC_TRNS, DRAG_SCROLL,
 
-                 KC_F12,  KC_F6,    KC_F7,         KC_F8,    KC_F9,          KC_F10, KC_TRNS,
-                 KC_PGUP,  KC_HOME, LALT(KC_LEFT), KC_UP,    LALT(KC_RIGHT), KC_NUM, KC_TRNS,
-                 KC_PGDN,  KC_END,  KC_LEFT,       KC_DOWN,  KC_RIGHT,       KC_NO,  KC_TRNS,
-        KC_NO,             KC_TRNS, LGUI(KC_LEFT), KC_NO,    LGUI(KC_RIGHT), KC_INS, KC_TRNS,
-        KC_TRNS, KC_TRNS,           TG_PRECISE,    KC_TRNS,  KC_TRNS,         TO(0),  KC_TRNS
+                 KC_F12,  KC_F6,   KC_F7,         KC_F8,    KC_F9,           KC_F10,  KC_TRNS,
+                 KC_NUM,  KC_NO,   LALT(KC_LEFT), KC_UP,    LALT(KC_RIGHT),  KC_PGUP, KC_HOME,
+                 KC_INS,  KC_NO,   KC_LEFT,       KC_DOWN,  KC_RIGHT,        KC_PGDN, KC_END,
+        KC_NO,            KC_NO,   LGUI(KC_LEFT), KC_NO,    LGUI(KC_RIGHT),  KC_NO,   KC_TRNS,
+        KC_TRNS, KC_TRNS,          TG_PRECISE,    KC_TRNS,  KC_TRNS,         TO(0),   KC_TRNS
     ),
     [2] = LAYOUT_mos(
         KC_TRNS,    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, TO(3),
         KC_TRNS,    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-        KC_CAPS,    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+        KC_TRNS,    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
         TG_PRECISE, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,          KC_TRNS,
         KC_TRNS,    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,          KC_TRNS, KC_TRNS,
 
@@ -62,9 +62,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_MS_BTN1, KC_MS_BTN2,                       KC_TRNS,    KC_TRNS, KC_TRNS, KC_TRNS, TO(0)
     ),
     [3] = LAYOUT_conf(
-        QK_BOOT, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, DT_PRNT, TO(0),
-        NK_ON,   NK_OFF,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, DT_UP,
-        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, DT_DOWN,
+        QK_BOOT, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, TO(0),
+        NK_ON,   NK_OFF,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
         KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,          KC_TRNS,
         KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,          KC_TRNS, KC_TRNS,
 
