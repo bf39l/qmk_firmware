214aa79ffcd106d658c2fa48be925eded764fe79
Bo Liu <boliu1234@gmail.com>
2024-12-19 13:26:01 +1300

    add via support

--- keyboards/alix_split/keymaps/default/keymap.c
+++ keyboards/alix_split/keymaps/default/keymap.c
@@ -32,7 +32,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                      KC_BSLS,        KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_EQL,
                      KC_QUOT,        KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_ENT,
         KC_NO,                       KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
-        DRAG_SCROLL, MT(1, KC_SPC),        KC_RGUI, KC_RALT, KC_RCTL, TO(1),   TO(2)
+        DRAG_SCROLL, LT(1, KC_SPC),        KC_RGUI, KC_RALT, KC_RCTL, TO(1),   TO(2)
 
     ),
     [1] = LAYOUT_fn(
